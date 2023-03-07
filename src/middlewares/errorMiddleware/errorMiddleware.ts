c5f33bee5c80b09bd681918c958d02ca3ed8433e
Marcel Martino <marcelmartino2053@gmail.com>
2023-03-07 15:48:55 +0100

    Create generalError middleware  and its test

--- src/middlewares/errorMiddleware/errorMiddleware.ts
+++ src/middlewares/errorMiddleware/errorMiddleware.ts
@@ -3,6 +3,18 @@ import createDebug from "debug";
 import CustomError from "../../customError/CustomError";
 const debug = createDebug("dronezone-api:middlewares");
 
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  debug(error.message);
+  res
+    .status(error.statusCode || 500)
+    .json({ error: error.publicMessage || "Something went wrong" });
+};
+
 export const notFoundError = (
   req: Request,
   res: Response,
