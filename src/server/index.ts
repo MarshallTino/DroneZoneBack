1ab6374bb77d7b02b2d9575887131c99cb3054e2
Marcel Martino <marcelmartino2053@gmail.com>
2023-03-07 11:41:53 +0100

    Create server app on server index implement cors and morgan

---
+++ src/server/index.ts
@@ -0,0 +1,13 @@
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.disable("x-powered-by");
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+export default app;
