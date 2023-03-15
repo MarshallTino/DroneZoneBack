a1f14bfb449571a581ad1038b6654c92f3e9977d
Marcel Martino <marcelmartino2053@gmail.com>
2023-03-15 11:06:33 +0100

    Create getuserdrones controller and test

--- src/server/controllers/dronesController/dronesController.ts
+++ src/server/controllers/dronesController/dronesController.ts
@@ -1,6 +1,7 @@
 import { type NextFunction, type Request, type Response } from "express";
 import CustomError from "../../../customError/CustomError.js";
 import { Drone } from "../../../database/models/droneSchema.js";
+import { type CustomRequest } from "./types.js";
 
 export const getDrones = async (
   req: Request,
@@ -21,4 +22,22 @@ export const getDrones = async (
   }
 };
 
-export default getDrones;
+export const getUserDrones = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userDrones = await Drone.find({ creator: req.creator }).exec();
+
+    res.status(200).json({ userDrones });
+  } catch (error) {
+    const customError = new CustomError(
+      "Bad request",
+      400,
+      "Couldn't retrieve user's Drones"
+    );
+
+    next(customError);
+  }
+};
